3f7769246c0d20d0b8e02ce8f48e4eb40d42b335
Henrique Lustosa <henriquealrs@gmail.com>
2025-07-04 09:26:05 -0300

    Update config

---
+++ .config/hypr/random_wallpaper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WALLPAPER_DIR="$HOME/.config/hypr/wallpapers"
+
+if ! pgrep -x swww-daemon > /dev/null; then
+    swww init
+    sleep 1
+fi
+
+# random_wallpaper=$(find "$WALLPAPER_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.jpeg' \) | shuf -n 1)
+random_wallpaper=$(find "$WALLPAPER_DIR" -type f  | shuf -n 1)
+
+if [[ -n "$random_wallpaper" ]]; then
+    swww img "$random_wallpaper" --transition-type any --transition-duration 2
+fi
