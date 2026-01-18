92c1c4beb5d1d9e6ecb4290fb41eb3145ab5f0e1
Henrique Silva <henrique.silva@liquiditi.com.br>
2026-01-18 11:58:06 -0300

    Random wallpaper

--- scripts/install_ubuntu.sh
+++ scripts/install_ubuntu.sh
@@ -35,6 +35,7 @@ chmod +x "$HOME/.config/i3/dynamic-monitor.sh"
 chmod +x "$HOME/.config/i3/screenshot.sh"
 chmod +x "$HOME/.config/polybar/launch.sh"
 
+mv "$HOME/.config/i3/random_wallpaper.sh" "$HOME/.local/bin/"
 # curl -fsSL https://github.com/zen-browser/updates-server/raw/refs/heads/main/install.sh | sh
 
 
@@ -44,7 +45,10 @@ cp "$ROOT_DIR/autodisplay_ubuntu.sh" "$HOME/.local/bin/"
 mkdir -p "$HOME/.config/systemd/user"
 cp -r "$ROOT_DIR/../.config/systemd/user/auto-display.service" "$HOME/.config/systemd/user/"
 cp -r "$ROOT_DIR/../.config/systemd/user/auto-display.timer" "$HOME/.config/systemd/user/"
+cp -r "$ROOT_DIR/../.config/systemd/user/random-wallpaper.service" "$HOME/.config/systemd/user/"
+cp -r "$ROOT_DIR/../.config/systemd/user/random-wallpaper.timer" "$HOME/.config/systemd/user/"
 # Enable and Start
 systemctl --user daemon-reexec
 systemctl --user daemon-reload
 systemctl --user enable --now auto-display.timer
+systemctl --user enable --now random-wallpaper.timer
